f4f6c8917d8e3094aff09c6779794b77a042fe24
SuperYYT <1209897320@qq.com>
2021-07-28 23:10:22 +0800

    删除广告，增加二维码扫描功能 — NyanCatada

--- single-sites.php
+++ single-sites.php
@@ -58,12 +58,13 @@ include( 'templates/header-nav.php' );
 	    									<p><?php echo get_post_meta(get_the_ID(), '_sites_sescribe', true) ?></p>
                                          <?php 
                                          $m_post_link_url = $m_link_url ?: get_permalink($post->ID);
-                                         $qrurl="https://my.tv.sohu.com/user/a/wvideo/getQRCode.do?width=150&height=150&text=". $m_post_link_url;
                                          $qrname = "手机查看";
                                          if(get_post_meta(get_the_ID(), '_wechat_qr', true)){
                                             $qrurl=get_post_meta(get_the_ID(), '_wechat_qr', true);
                                             $qrname = "公众号";
-                                         } 
+                                         }
+
+                                         $qrurl = "https://api.pwmqr.com/qrcode/create/?url=".$m_post_link_url;
                                          ?>
 	    								<div class="site-go mt-3">
                                         <?php if($m_link_url!=""): ?>
